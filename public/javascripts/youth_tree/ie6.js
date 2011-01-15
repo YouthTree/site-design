faf2dabc2817167c54b102a67a757cf07e2001a3
Darcy Laycock <sutto@thefrontiergroup.com.au>
2011-01-16 04:56:44 +0800

    Refactor how the header is laid out

--- public/javascripts/youth_tree/ie6.js
+++ public/javascripts/youth_tree/ie6.js
@@ -1,9 +1,9 @@
-/* DO NOT MODIFY. This file was compiled Sat, 15 Jan 2011 19:34:42 GMT from
+/* DO NOT MODIFY. This file was compiled Sat, 15 Jan 2011 20:23:19 GMT from
  * /Users/sutto/Code/YouthTree/yt-redesign/coffeescripts/youth_tree/ie6.coffee
  */
 
 YouthTree.withNS('IE6', function(ns) {
-  ns.belatedPNGSelectors = ["#page-header h1 a", "ul#social-media-icons li a", "#yt-subscribe-button button"];
+  ns.belatedPNGSelectors = ["#page-header h1 a", "ul#social-media-icons li a", "#yt-subscribe-button button", "#flash-messages p.flash"];
   return ns.setup = function() {
     if (typeof DD_belatedPNG != "undefined" && DD_belatedPNG !== null) {
       return $(ns.belatedPNGSelectors.join(", ")).each(function() {
